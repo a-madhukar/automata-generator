c6d24678af5046d61b66f7dcea5109ce1f630a2a
a-madhukar <a.madhukar@yahoo.com>
2019-10-17 21:00:45 +0800

    refactor init method

--- app.js
+++ app.js
@@ -17,7 +17,65 @@ new Vue({
   
   methods:{
     init(){
-//       this.log.push("in the init method"); 
+
+      this.setupFirstRow(); 
+      
+      for (let row = 0; row < 50; row++) {
+        
+        let currentCells = this.cells[row]; 
+      
+        let nextRangeOfCells = []; 
+
+        console.log("Row NO: " + row); 
+        console.log("currentCells " + currentCells); 
+
+
+        for(let i = 0; i< currentCells.length; i++) {
+
+          let patternString =  this.getPatternString(
+            !currentCells[i - 1] ? 0 : currentCells[i - 1], 
+            currentCells[i], 
+            !currentCells[i + 1] ? 0 : currentCells[i + 1]
+          ); 
+
+          nextRangeOfCells.push(
+            this.getResultOfPattern(patternString)
+          ); 
+
+        }
+
+        this.cells.push(nextRangeOfCells); 
+
+        console.log(this.cells);  
+        
+      }
+      
+      
+    }, 
+
+
+    getPatternString(leftNo, currentNo, rightNo) {
+      return '' + leftNo + '' + currentNo + '' + rightNo;
+    }, 
+
+    getResultOfPattern(patternString) {
+      let results = {
+        '111':0, 
+        '110':0, 
+        '101':0,
+        '100':1, 
+        '011':1,
+        '010':1,
+        '001':1,
+        '000':0
+      }; 
+
+      return results[patternString]; 
+    }, 
+
+
+    setupFirstRow() {
+      //       this.log.push("in the init method"); 
       console.log("in the init method"); 
 
       let firstRow = []; 
@@ -31,80 +89,9 @@ new Vue({
       firstRow.splice(Math.round(totalCells/2), 1, 1); 
 
       this.cells.push(firstRow); 
-
-      
-      for (let row = 0; row < 50; row++) {
-        
-        let currentCells = this.cells[row]; 
-      
-        let nextRangeOfCells = []; 
-
-
-        for(let i = 0; i< currentCells.length; i++) {
-
-          let currentNo = currentCells[i]; 
-          let leftNo = !currentCells[i - 1] ? 0 : currentCells[i - 1]; 
-          let rightNo = !currentCells[i + 1] ? 0 : currentCells[i + 1]; 
-          let result = 0; 
-
-          console.log("current no: " + currentCells[i]); 
-          console.log(leftNo, currentNo, rightNo); 
-
-          // rule 1 
-          if (leftNo === 1 && currentNo == 1 && rightNo == 1)
-            result = 0;
-
-          // rule 2
-          if (leftNo == 1 && currentNo == 1 && rightNo == 0)
-            result = 0; 
-
-          // rule 3
-          if (leftNo == 1 && currentNo == 0 && rightNo == 1)
-            result = 0; 
-
-          // rule 4
-          if (leftNo == 1 && currentNo == 0 && rightNo == 0)
-            result = 1; 
-
-          // rule 5
-          if(leftNo == 0 && currentNo == 1 && rightNo == 1)
-            result = 1; 
-
-
-          // rule 6
-          if (leftNo == 0 && currentNo == 1 && rightNo == 0)
-            result = 1; 
-
-          // rule 7
-          if (leftNo == 0 && currentNo == 0 && rightNo == 1)
-            result = 1; 
-
-          // rule 8
-          if (leftNo == 0 && currentNo == 0 && rightNo == 0)
-            result = 0; 
-
-
-          console.log("Result: " + result); 
-          nextRangeOfCells.push(result); 
-
-        }
-
-        console.log("next range of cells: ", nextRangeOfCells); 
-
-
-        this.cells.push(nextRangeOfCells); 
-
-          console.log(this.cells);
-
-       
-        
-        
-      }
-      
-       
-   
-      
     }, 
+
+
   }, 
   
   mounted(){
