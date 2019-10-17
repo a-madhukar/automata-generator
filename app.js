24165f92e50d91b5b4025f1e2359093612dd4269
a-madhukar <a.madhukar@yahoo.com>
2019-10-18 04:33:34 +0800

    generate automata for the whole screen

--- app.js
+++ app.js
@@ -1,56 +1,122 @@
 new Vue({
   el: '#app', 
   
-  data(){
-    return {
-      cells: [], 
-      
-      previousRange:[], 
-      
-      currentRange:[], 
-      
-      
-      log:[], 
-      
-    }; 
-  }, 
-  
   methods:{
-    init(){
+    init() {
 
-      this.setupFirstRow(); 
-      
-      for (let row = 0; row < 50; row++) {
-        
-        let currentCells = this.cells[row]; 
-      
-        let nextRangeOfCells = []; 
-
-        console.log("Row NO: " + row); 
-        console.log("currentCells " + currentCells); 
+      let cellsBoard = document.querySelector(".cells__board");
 
 
-        for(let i = 0; i< currentCells.length; i++) {
+      // draw the rows
+      for (let i = 1; i <= this.getNumberOfRowsToDraw(); i++) {
 
-          let patternString =  this.getPatternString(
-            !currentCells[i - 1] ? 0 : currentCells[i - 1], 
-            currentCells[i], 
-            !currentCells[i + 1] ? 0 : currentCells[i + 1]
-          ); 
+        let cellsRow = document.createElement("div"); 
 
-          nextRangeOfCells.push(
-            this.getResultOfPattern(patternString)
-          ); 
+        cellsRow.classList.add('cells__row'); 
+
+        cellsBoard.append(cellsRow);  
+
+      }
+
+
+      // draw the cells 
+      let cellRows = document.querySelectorAll('.cells__row'); 
+
+      for (let index in cellRows) {
+
+        for (let i = 1; i <= this.getNumberOfCellsToDraw(cellRows[index]); i++) {
+
+          let cell = document.createElement('div'); 
+
+
+          cell.classList.add('cells__row-cell'); 
+
+          cell.classList.add(Math.round(Math.random()) ? 'background--on' : 'background--off'); 
+
+          cellRows[index].append(cell); 
 
         }
 
-        this.cells.push(nextRangeOfCells); 
-
-        console.log(this.cells);  
-        
       }
       
-      
+    }, 
+
+
+    generatePattern () {
+
+      let rows = document.querySelectorAll('.cells__row'); 
+
+      // set first row cells to close state. and turn 1 state on 
+
+      let firstRowCells = rows[0].childNodes; 
+
+      this.setAllCellsToFalse(firstRowCells);
+
+      firstRowCells[Math.round(firstRowCells.length/2)].classList.add('background--on');
+
+      for (let i=1; i<rows.length; i++) {
+
+        let previousRow = rows[i -1]; 
+        let currentRow = rows[i]; 
+        let nextRow = rows[i+1]; 
+        let currentRowCells = currentRow.childNodes; 
+        let previousRowCells = previousRow.childNodes; 
+
+        // set current row cells to false 
+        this.setAllCellsToFalse(currentRowCells); 
+
+        for (let i = 0; i < previousRowCells.length; i++) {
+
+          let leftCell = previousRowCells[i-1];
+          let currentCell = previousRowCells[i];
+          let rightCell = previousRowCells[i+1];
+
+          let leftCellVal = !leftCell ? 0 : +leftCell.classList.contains('background--on'); 
+          let currentCellVal = !currentCell ? 0 : + currentCell.classList.contains('background--on'); 
+          let rightCellVal = !rightCell ? 0 :  + rightCell.classList.contains('background--on'); 
+
+          // console.log("cell values"); 
+          // console.table([leftCellVal, currentCellVal, rightCellVal]); 
+
+          let patternString = this.getPatternString(leftCellVal, currentCellVal, rightCellVal); 
+
+          let result = this.getResultOfPattern(patternString); 
+
+          currentRowCells[i].classList.add(result ? 'background--on' : 'background--off'); 
+
+          // debugger; 
+
+        }
+
+
+      } 
+
+    }, 
+
+    setAllCellsToFalse (cells) {
+
+      for (let i = 0; i<cells.length; i++) {
+        if (cells[i].classList.contains('background--on')) {
+          cells[i].classList.remove('background--on'); 
+          cells[i].classList.add('background--off'); 
+        }
+          
+      }
+
+    }, 
+
+    getNumberOfRowsToDraw () {
+
+      let containerHeight = document.querySelector('.cells__board').offsetHeight;
+
+
+      return containerHeight/12; 
+    }, 
+
+    getNumberOfCellsToDraw (row) {
+
+      return row.offsetWidth / 10; 
+
     }, 
 
 
@@ -71,24 +137,6 @@ new Vue({
       }; 
 
       return results[patternString]; 
-    }, 
-
-
-    setupFirstRow() {
-      //       this.log.push("in the init method"); 
-      console.log("in the init method"); 
-
-      let firstRow = []; 
-      let totalCells = 80; 
-
-      //setup the first row of the cell 
-      for(let count = 1; count <= totalCells; count ++) {
-        firstRow.push(0); 
-      }
-
-      firstRow.splice(Math.round(totalCells/2), 1, 1); 
-
-      this.cells.push(firstRow); 
     }, 
 
 
