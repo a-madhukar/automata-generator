b3a917178965ed1434c7833df47675116437da8c
a-madhukar <a.madhukar@yahoo.com>
2019-10-28 11:35:29 +0800

    make production ready

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    mode:'development', 
+    mode:'production', 
     entry: './src/app.js',
     output: {
         path:path.resolve(__dirname, 'dist'), 
